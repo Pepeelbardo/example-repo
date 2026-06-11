cd0eab74621daf41ccd19f4ce1350ffa50698070
Pepeelbardo <jlsarabian@oulook.com>
2026-06-10 21:57:58 -0500

    added git clone description

--- conversion.py
+++ conversion.py
@@ -2,12 +2,12 @@ num1 = 99.23 #into an integer
 num2 = 23 # into a float 
 num3 = 150 #into a String
 string1 = "100" #inton an integer
-#convertting 
+#convertting .
 num1 = int(num1)
 num2 = float(num2)
 num3 = str(num3)
 string1 = int(string1)
-#printing each variable into a single line
+#printing each variable into a single line.
 print(num1)
 print(num2)
 print(num3)
